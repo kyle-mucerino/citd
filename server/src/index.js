c0772fab41a5d0453fce84e093ce189a7cf9b4b8
kyle-mucerino <kyle.mucerino@gmail.com>
2024-09-03 12:56:05 -0700

    ful stack deploy 2

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
@@ -13,10 +14,17 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
-const subscriptionRoutes = require("./routes");
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../../build")));
 
+// API Routes
+const subscriptionRoutes = require("./routes");
 app.use("/api", subscriptionRoutes); // Use subscription routes
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../build", "index.html"));
+});
 
 // Connect to MongoDB
 mongoose
@@ -24,6 +32,7 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
