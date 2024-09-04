6ec20d45cf0529dacb28848fe251dd9cba43ff89
kyle-mucerino <kyle.mucerino@gmail.com>
2024-09-03 17:14:15 -0700

    mongodb uri change

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,7 @@ app.get("*", (req, res) => {
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
