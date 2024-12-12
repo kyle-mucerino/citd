3617bee2b68bc04c3f807096d23d60e36f2ba34b
kyle-mucerino <kyle.mucerino@gmail.com>
2024-12-11 19:29:55 -0800

    lamp effect homepage

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { CanvasRevealEffectDemo } from './CanvasRevealEffectDemo';
 import { VortexDemo } from './VortexDemo';
 import { AnimatedTestimonialsDemo } from './AnimatedTestimonialsDemo';
+import { LampDemo } from './lampDemo';
+import { SparklesPreview } from './sparklesPreview';
 
 const Home = () => {
   return (
     <div className="bg-black">
-      <VortexDemo />
+      {/* <VortexDemo /> */}
+      {/* <SparklesPreview /> */}
+      <LampDemo />
       <AnimatedTestimonialsDemo />
       <CanvasRevealEffectDemo />
     </div>
