f8cacae01d7e8e7c273472fbe91d34f6a3b71b86
kyle-mucerino <kyle.mucerino@gmail.com>
2025-01-06 10:28:16 -0800

    helmet wrapped on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Home from './Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async"; // Ensure HelmetProvider wraps the app
+import "./index.css";
+import Home from "./Home";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <HelmetProvider>
+      <Home />
+    </HelmetProvider>
   </React.StrictMode>
 );
