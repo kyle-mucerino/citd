63510e33897024f92463d4623c42aae4ef4c6caf
kyle-mucerino <kyle.mucerino@gmail.com>
2025-01-06 10:26:14 -0800

    helmet installed

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import { CanvasRevealEffectDemo } from './CanvasRevealEffectDemo';
-import { VortexDemo } from './VortexDemo';
-import { AnimatedTestimonialsDemo } from './AnimatedTestimonialsDemo';
-import { LampDemo } from './lampDemo';
-import { SparklesPreview } from './sparklesPreview';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { CanvasRevealEffectDemo } from "./CanvasRevealEffectDemo";
+import { AnimatedTestimonialsDemo } from "./AnimatedTestimonialsDemo";
+import { LampDemo } from "./lampDemo";
 
 const Home = () => {
   return (
     <div className="bg-black">
-      {/* <VortexDemo /> */}
-      {/* <SparklesPreview /> */}
+      {/* Helmet for SEO */}
+      <Helmet>
+        <title>Crossing i's Dotting t's - Official Band Website</title>
+        <meta
+          name="description"
+          content="Welcome to the official website of Crossing i's Dotting t's. Discover our music, read testimonials, and explore unique content."
+        />
+        <meta
+          name="keywords"
+          content="Crossing i's Dotting t's, band, music, testimonials, creative effects"
+        />
+        <link rel="canonical" href="https://cidtband.com" />
+      </Helmet>
+
+      {/* Page Content */}
       <LampDemo />
       <AnimatedTestimonialsDemo />
       <CanvasRevealEffectDemo />
     </div>
   );
-} 
+};
 
 export default Home;
