bb69d1df5c23d556b743b20a8c61eca44fd414aa
kyle-mucerino <kyle.mucerino@gmail.com>
2025-01-06 21:23:59 -0800

    remove headers

--- src/lampDemo.js
+++ src/lampDemo.js
@@ -17,19 +17,9 @@ export function LampDemo() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl flex flex-col"
       >
-        <img
-          className=""
-          alt="Crossing i's Dotting t's logo"
-          src={vortexLogo}
-        />
-        <h1 className="text-xl sm:text-5xl reenie-beanie-regular">
-          Crossing i's Dotting t's - Official Website
-        </h1>
-        <h2 className="text-lg sm:text-4xl reenie-beanie-regular">
-          Welcome to the official website of Crossing i's Dotting t's. Discover
-          our latest music, videos, tour dates, and more!
-        </h2>
-        <h3 className="text-xl">&darr;</h3>
+        <img className="" alt="Crossing i's Dotting t's logo" src={vortexLogo} />
+        <div className="text-5xl reenie-beanie-regular">Explore More</div>
+        <div className="text-xl">&darr;</div>
       </motion.h1>
     </LampContainer>
   );
