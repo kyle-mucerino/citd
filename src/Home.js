7d973da341f061e9f5bee344082eecde6f6baaca
kyle-mucerino <kyle.mucerino@gmail.com>
2026-07-20 19:06:22 -0500

    summer tour poster as jpg

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ import cassette from "./assets/cassette.png";
 import flyer01 from "./assets/flyer01.png";
 import flyer02 from "./assets/Newpic925.jpeg";
 import vinyl from "./assets/vinyl.png";
-import tourPoster from "./assets/tourPosterSummer26.png";
+import tourPoster from "./assets/SummerTourPoster.jpg";
 
 const Home = () => {
   const [email, setEmail] = useState("");
